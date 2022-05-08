a05245bbd09826e050c573d0b9d9325e2c0acfce
Masoom Choudhary <Masoom Choudhary@CHOUDHARY>
2022-05-08 22:29:40 +0530

    array

---
+++ core.java/src/array/D2array.java
@@ -0,0 +1,26 @@
+package array;
+
+public class D2array {
+
+	public static void main(String[] args)
+	{
+		int arr[][] = new int[2][];
+		arr[0] = new int[3];
+		arr[1] = new int[2];
+		
+		int count = 0;
+		for(int i=0; i<arr[i].length; i++)
+			for(int j=0; j<arr[i].length; j++)
+				arr[i][j] = count++;
+		System.out.print("Contents of 2D jagged Array");
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; i<arr[i].length; j++)
+				System.out.print(arr[i][j] + " ");
+			System.out.println();
+				
+		}
+		
+	}
+
+}
---
+++ core.java/src/array/Singlearray.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Singlearray {
+
+	public static void main(String[] args)
+	{
+		int a[] =new int[5]; //declaraction and instantitation
+		a[0] = 10;
+		a[1] = 15;
+		a[2] = 20;
+		a[3] = 25;
+		a[4] = 30;
+		
+		for (int i =0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
